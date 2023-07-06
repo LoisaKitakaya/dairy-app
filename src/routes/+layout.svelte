a6e43a1eb54ac333c44385fca9f0e07a9868138a
Freedom Loisa <kitakayaloisa@gmail.com>
2023-07-07 02:43:48 +0300

    created basic app layout

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,4 +2,136 @@
 	import '../app.css';
 </script>
 
+<!-- Navbar -->
+<div class="navbar bg-base-200 shadow-sm fixed top-0">
+	<div class="navbar-start">
+		<div class="dropdown">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<span tabindex="0" class="btn btn-ghost lg:hidden">
+				<i class="bi bi-list text-2xl" />
+			</span>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content mt-4 z-[1] p-2 bg-base-200 shadow-sm rounded-box w-36"
+			>
+				<li>
+					<a href="/" class="flex items-center justify-start gap-2"
+						><i class="bi bi-house text-lg" /> <span>Home</span></a
+					>
+				</li>
+				<li>
+					<a href="/production" class="flex items-center justify-start gap-2"
+						><i class="bi bi-droplet text-lg" /> <span>Production</span></a
+					>
+				</li>
+				<li>
+					<a href="/earnings" class="flex items-center justify-start gap-2"
+						><i class="bi bi-piggy-bank text-lg" /> <span>Earnings</span></a
+					>
+				</li>
+				<li>
+					<a href="/customer" class="flex items-center justify-start gap-2"
+						><i class="bi bi-people text-lg" /> <span>Customers</span></a
+					>
+				</li>
+				<li>
+					<a href="/expenses" class="flex items-center justify-start gap-2"
+						><i class="bi bi-cash text-lg" /> <span>Expenses</span></a
+					>
+				</li>
+				<li>
+					<a href="/reports" class="flex items-center justify-start gap-2"
+						><i class="bi bi-bar-chart-line text-lg" /> <span>Reports</span></a
+					>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="navbar-center hidden lg:flex">
+		<ul class="flex items-center justify-center gap-4 px-1">
+			<li>
+				<a href="/" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-house text-lg" /> <span>Home</span></a
+				>
+			</li>
+			<li>
+				<a href="/production" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-droplet text-lg" /> <span>Production</span></a
+				>
+			</li>
+			<li>
+				<a href="/earnings" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-piggy-bank text-lg" /> <span>Earnings</span></a
+				>
+			</li>
+			<li>
+				<a href="/customers" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-people text-lg" /> <span>Customers</span></a
+				>
+			</li>
+			<li>
+				<a href="/expenses" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-cash text-lg" /> <span>Expenses</span></a
+				>
+			</li>
+			<li>
+				<a href="/reports" class="flex items-center justify-start gap-1 link link-hover"
+					><i class="bi bi-bar-chart-line text-lg" /> <span>Reports</span></a
+				>
+			</li>
+		</ul>
+	</div>
+	<div class="navbar-end">
+		<div class="dropdown dropdown-end">
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<span tabindex="0" class="btn btn-ghost btn-circle">
+				<i class="bi bi-three-dots text-xl sm:text-2xl" />
+			</span>
+			<!-- svelte-ignore a11y-no-noninteractive-tabindex -->
+			<ul
+				tabindex="0"
+				class="menu menu-sm dropdown-content mt-4 z-[1] p-2 bg-base-200 shadow-sm rounded-box w-36"
+			>
+				<li>
+					<a href="/profile" class="flex items-center justify-start gap-2"
+						><i class="bi bi-person-circle text-lg" /> <span>Profile</span></a
+					>
+				</li>
+				<li>
+					<a href="/themes" class="flex items-center justify-start gap-2"
+						><i class="bi bi-palette text-lg" /> <span>Themes</span></a
+					>
+				</li>
+				<li>
+					<form action="" method="POST">
+						<button type="submit" class="flex items-center justify-start gap-2"
+							><i class="bi bi-box-arrow-left text-lg" /> <span>Logout</span></button
+						>
+					</form>
+				</li>
+			</ul>
+		</div>
+	</div>
+</div>
+<!-- Navbar -->
+
 <slot />
+
+<!-- Footer -->
+<footer class="footer footer-center p-10 bg-base-200 text-base-content rounded w-full">
+	<div class="grid grid-flow-row sm:grid-flow-col gap-2 sm:gap-4">
+		<a href="/" class="link link-hover">Home</a>
+		<a href="/production" class="link link-hover">Production</a>
+		<a href="/earnings" class="link link-hover">Earnings</a>
+		<a href="/customers" class="link link-hover">Customers</a>
+		<a href="/expenses" class="link link-hover">Expenses</a>
+		<a href="/reports" class="link link-hover">Reports</a>
+	</div>
+	<div>
+		<p>
+			Copyright © 2023 - All right reserved - <span class="font-semibold">Rislo Farm</span> Ltd
+		</p>
+	</div>
+</footer>
+<!-- Footer -->
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,2 @@
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
+<section class="pt-20 pb-8 px-6 sm:px-4">
+</section>
