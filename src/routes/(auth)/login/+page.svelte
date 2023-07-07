720d98265b395cc9c2f77a9e0cec04652660a642
Freedom Loisa <kitakayaloisa@gmail.com>
2023-07-07 16:42:23 +0300

    created auth pages

--- src/routes/(auth)/login/+page.svelte
+++ src/routes/(auth)/login/+page.svelte
@@ -1,2 +1,47 @@
-<section class="pt-20 pb-8 px-6 sm:px-4">
-</section>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 items-center">
+	<div class="mx-auto max-w-sm">
+		<h2 class="text-2xl font-bold leading-9 tracking-tight">Sign in to your account</h2>
+	</div>
+	<div class="mt-10 mx-auto max-w-sm">
+		<form class="space-y-6 bg-base-200 px-4 py-6 rounded-md" action="#" method="POST">
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6">Username</label>
+				<div class="mt-2">
+					<input
+						type="text"
+						name="username"
+						placeholder="e.g. John Doe"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<div class="flex items-center justify-between w-full max-w-xs">
+					<label for="password" class="block text-sm font-medium leading-6">Password</label>
+					<div class="text-sm">
+						<a href="/reset" class="font-semibold link">Forgot password?</a>
+					</div>
+				</div>
+				<div class="mt-2">
+					<input
+						type="password"
+						name="password"
+						required
+						placeholder="password"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<button type="submit" class="btn btn-success w-full max-w-xs">Sign In</button>
+			</div>
+		</form>
+	</div>
+	<div class="mt-4 mx-auto max-w-sm">
+		<p>
+			Don't have an account? <a href="/register" class="link font-semibold">Register</a> here.
+		</p>
+	</div>
+</div>
--- src/routes/(auth)/register/+page.svelte
+++ src/routes/(auth)/register/+page.svelte
@@ -1,2 +1,62 @@
-<section class="pt-20 pb-8 px-6 sm:px-4">
-</section>
+<div class="flex min-h-full flex-col justify-center px-6 py-12 items-center">
+	<div class="mx-auto max-w-sm">
+		<h2 class="text-2xl font-bold leading-9 tracking-tight">Sign up for a new account</h2>
+	</div>
+	<div class="mt-10 mx-auto max-w-sm">
+		<form class="space-y-6 bg-base-200 px-4 py-6 rounded-md" action="#" method="POST">
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6">Username</label>
+				<div class="mt-2">
+					<input
+						type="text"
+						name="username"
+						placeholder="e.g. John Doe"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+			<div>
+				<label for="email" class="block text-sm font-medium leading-6">Email address</label>
+				<div class="mt-2">
+					<input
+						type="email"
+						name="email"
+						placeholder="e.g. johndoe@example.com"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<label for="password" class="block text-sm font-medium leading-6">Password</label>
+				<div class="mt-2">
+					<input
+						type="password"
+						name="password"
+						required
+						placeholder="password"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+				<div class="mt-2">
+					<input
+						type="password"
+						name="confirmPassword"
+						required
+						placeholder="confirm password"
+						class="input input-bordered w-full max-w-xs"
+					/>
+				</div>
+			</div>
+
+			<div>
+				<button type="submit" class="btn btn-success w-full max-w-xs">Sign Up</button>
+			</div>
+		</form>
+	</div>
+	<div class="mt-4 mx-auto max-w-sm">
+		<p>
+			Already have an account? <a href="/login" class="link font-semibold">Login</a> here.
+		</p>
+	</div>
+</div>
